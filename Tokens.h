7e59293d956fbeebacd86ec07e7d4e823df956e3
Sayed-badr <68440283+Sayed-badr@users.noreply.github.com>
2021-07-13 22:01:08 +0200

    Update Tokens.h

--- Tokens.h
+++ Tokens.h
@@ -20,6 +20,15 @@ private:
 public:
 	void set_name(string s);
 	string get_name();
+	
+	void set_type(tokenType t);
+	tokenType get_type();
+
+	void add_attribute(Attribute t);
+	list <Attribute> get_attributes();
+
+	void add_data(string t);
+	list <string> get_data();
 };
 
 #endif /* TOKENS_H_ */
